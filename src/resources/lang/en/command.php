0cca6c708ae7cafd6fb20396fd46505f6adc5768
Al Lestaire Acasio <allestaire.acasio@ustp.edu.ph>
2023-02-25 00:02:51 +0800

    :globe_with_meridians: Creating locale

--- src/resources/lang/en/command.php
+++ src/resources/lang/en/command.php
@@ -3,6 +3,8 @@
 return [
   'create' => [
     'creating' => 'Creating module of [:package].',
-    'making_dir' => 'Making directory.'
+    'making_dir' => 'Making directory.',
+    'installed' => 'Module already installed [:package]',
+    'install' => 'Module registered. Run [composer require :package] to instsall.'
   ]
 ];
